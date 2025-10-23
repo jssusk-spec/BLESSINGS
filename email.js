0097cf16ae6410780ebc0a652584e6e738417c60
jssusk-spec <jssusk@gmail.com>
2025-10-22 18:15:18 -0700

    Update email.js

--- email.js
+++ email.js
@@ -1,5 +1,3 @@
-// Mock serverless function for sending thank-you emails via SendGrid
 module.exports = async (req, res) => {
-  // Requires SendGrid API key in production
-  res.json({ message: "Thank you email sent!" });
+  res.status(200).json({ message: "Thank you email sent!" });
 };
